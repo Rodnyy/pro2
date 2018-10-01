12b272d5c197aeb69597170fa573b98b0ebb742e
suchoja2 <j.suchomel3702@gmail.com>
2018-10-01 13:49:51 +0200

    Vytvoření základní editace tabulky

--- src/ui/ProFrame.java
+++ src/ui/ProFrame.java
@@ -1,12 +1,20 @@
 package ui;
 
+import model.TableModel;
+import model.ToDoItem;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.List;
 
 public class ProFrame extends JFrame {
 
     static int width = 600;
     static int height = 600;
+    private TableModel model;
 
     public static void main(String... args) {
 
@@ -28,8 +36,71 @@ public class ProFrame extends JFrame {
         button.setText("Přidat poznámku");
         toolbar.add(button);
 
-        JTable table = new JTable();
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ToDoItem item = new ToDoItem("Test");
+                model.add(item);
+            }
+        });
+
+        JButton buttSave = new JButton();
+        buttSave.setText("Ulož");
+        toolbar.add(buttSave);
+
+        buttSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                saveItems();
+            }
+        });
+
+        JButton buttLoad = new JButton();
+        buttLoad.setText("Načti");
+        toolbar.add(buttLoad);
+
+        buttLoad.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                loadItems();
+            }
+        });
+
+        model = new TableModel();
+
+        JTable table = new JTable(model);
         add(new JScrollPane(table),BorderLayout.CENTER);
         pack();
+
+        setLocationRelativeTo(null);
+    }
+
+    private void saveItems(){
+        try{
+            ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(new File("our.db")));
+            stream.writeObject(model.getItems());
+            stream.flush();
+            stream.close();
+
+            }catch (Exception e){
+
+            e.printStackTrace();
+
+            }
+
+    }
+
+    private void loadItems(){
+        try{
+            ObjectInputStream stream = new ObjectInputStream(new FileInputStream(new File("our.db")));
+            List<ToDoItem> items = (List<ToDoItem>) stream.readObject();
+            stream.close();
+            model.setItems(items);
+            model.fireTableDataChanged();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
+
